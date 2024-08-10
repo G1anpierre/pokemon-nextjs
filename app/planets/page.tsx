5259581d409b55d714e55f764adcc0023464d8b5
G1anpierre <fgianpierre@gmail.com>
2024-08-10 16:35:09 +0200

    feat: add some small fixes

---
+++ app/planets/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlanetsPage = () => {
+  return <div>PlanetsPage in Construction</div>;
+};
+
+export default PlanetsPage;
--- config/site.ts
+++ config/site.ts
@@ -5,24 +5,12 @@ export const siteConfig = {
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
-      label: "Home",
+      label: "Characters",
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
+      label: "Planets",
+      href: "/planets",
     },
   ],
   navMenuItems: [
@@ -60,10 +48,6 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui.org",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
+    github: "https://github.com/G1anpierre",
   },
 };
